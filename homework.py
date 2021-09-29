7d885f054bac012828320db42b6042b804985d93
Nikolay <red.boad@yandex.ru>
2021-09-29 17:16:22 +0300

    еще раз

--- homework.py
+++ homework.py
@@ -20,44 +20,44 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
-        
+    
     def add_record(self, record):
         self.records.append(record)
-         
+    
     def get_today_stats(self):
         amount_all = 0
         for i in self.records:
-            if i.date == Calculator.day_now:   
-                amount_all += i.amount        
+            if i.date == Calculator.day_now:
+                amount_all += i.amount
         return amount_all
-        
+    
     def get_week_stats(self):
         amount_all = 0
-        date_ago = Calculator.day_now - dt.timedelta(days=7)     
+        date_ago = Calculator.day_now - dt.timedelta(days=7)
         for i in self.records:
-            if i.date >= date_ago and i.date <= Calculator.day_now : 
+            if i.date >= date_ago and i.date <= Calculator.day_now:
                 amount_all += i.amount
-        return amount_all 
+        return amount_all
 
 
 class CaloriesCalculator(Calculator):
 
-    def get_calories_remained(self):     
-        rest = self.limit - self.get_today_stats()        
+    def get_calories_remained(self):
+        rest = self.limit - self.get_today_stats()
         if rest > 0:
             return (f'Сегодня можно съесть что-нибудь ещё, но с '
                     f'общей калорийностью не более {rest} кКал')
         else:
-            return f'Хватит есть!'    
-    
+            return f'Хватит есть!'
+
 
 class CashCalculator(Calculator):
     USD_RATE = 60.0
     EURO_RATE = 80.0
 
-    def get_today_cash_remained(self, currency):     
+    def get_today_cash_remained(self, currency):
         curren = 'руб'
-        rest = self.limit - self.get_today_stats()          
+        rest = self.limit - self.get_today_stats() 
         if rest != 0:
             if currency == 'eur':
                 rest = rest / CashCalculator.EURO_RATE
